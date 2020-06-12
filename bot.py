85be3cd9b836ac73ca1f0e60f9bd223fc79ae350
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2020-06-12 13:38:29 -0500

    playing with the bot

--- bot.py
+++ bot.py
@@ -1,25 +1,35 @@
 import tweepy
-import os
-os.chdir("./keys.py")
 
-auth = tweepy.OAuthHandler (os.consumer_key, os.consumer_secret)
-auth.set_access_token (os.access_token, os.access_token_secret)
+from keys import keys
 
+import time
+
+consumer_key = keys['consumer_key']
+consumer_secret = keys['consumer_secret']
+access_token = keys['access_token']
+access_token_secret = keys['access_token_secret']
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 user = api.me()
 print(user.name)
+print(user.screen_name)
+print(user.followers_count)
+for friend in user.friends():
+    print(friend.screen_name)
 
 def main():
-  search = ("python","coding","NUNCAPARESDEAPRENDER")
-  numberOfTweets = 5
-  for tweet in tweepy.Cursor(api.search).items(numberOfTweets):
-    try:
-      tweet.retweet()
-      print("Tweet Retweeted")
-    except tweepy.TweepError as e:
-      print(e.reason)
-    except StopIteration:
-      break
+    search = ("@cargdev" )
 
-main()
+    numberofTweets = 1
+    for tweet in tweepy.Cursor(api.search, search).items(numberofTweets):
+        try:
+            tweet.retweet()
+            print("Tweet Retweeted")
+        except tweepy.TweepError as e:
+            print(e.reason)
+        except StopIteration:
+            break
+# main()
